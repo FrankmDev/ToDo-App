eba9fafe40a5d8eebbf2d64fae892a2624cedae9
FrankmDev <frankmmarindev@gmail.com>
2023-08-12 14:06:27 +0200

    ToDo List 2.0

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+let input = document.querySelector(".inputTask");
+let addTask = document.querySelector(".addTask");
+let list = document.querySelector("ul");
+let empty = document.querySelector(".empty");
+
+addTask.addEventListener("click", function (e) {
+  const textInput = input.value;
+  const listItem = document.createElement("li");
+  const p = document.createElement("p");
+  p.textContent = textInput;
+  listItem.appendChild(p);
+  list.appendChild(listItem);
+});
