6e66943bae803642286982dd420eabd830efc92c
FrankmDev <frankmmarindev@gmail.com>
2023-09-03 22:32:51 +0200

    Bugs finished

--- script.js
+++ script.js
@@ -5,18 +5,20 @@ let empty = document.querySelector(".empty");
 
 addTask.addEventListener("click", function (e) {
   const textInput = input.value;
-  const listItem = document.createElement("li");
-  const p = document.createElement("p");
-  p.textContent = textInput;
-  listItem.appendChild(p);
-  list.appendChild(listItem);
+  if (textInput.trim() !== "") {
+    const listItem = document.createElement("li");
+    const p = document.createElement("p");
+    p.textContent = textInput;
+    listItem.appendChild(p);
+    list.appendChild(listItem);
 
-  let removeBtn = document.createElement("button");
-  removeBtn.textContent = "X";
-  removeBtn.className = "removeBtn";
-  listItem.appendChild(removeBtn);
+    let removeBtn = document.createElement("button");
+    removeBtn.textContent = "X";
+    removeBtn.className = "removeBtn";
+    listItem.appendChild(removeBtn);
 
-  removeBtn.addEventListener("click", function () {
-    list.removeChild(listItem);
-  });
+    removeBtn.addEventListener("click", function () {
+      list.removeChild(listItem);
+    });
+  }
 });
